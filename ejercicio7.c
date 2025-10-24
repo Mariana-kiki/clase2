0ad2c6c79d10bfaceee934beac710ac39445ee05
Mariana-kiki <acosmariana9@outlook.com>
2025-10-24 09:14:10 -0600

    ejercicio7.c

---
+++ ejercicio7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+
+    printf("Introduce tres números de una cifra: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if ((a == 1 && b == 2 && c == 3) || (a == 1 && c == 2 && b == 3) ||
+        (b == 1 && a == 2 && c == 3) || (b == 1 && c == 2 && a == 3) ||
+        (c == 1 && a == 2 && b == 3) || (c == 1 && b == 2 && a == 3))
+        printf("Acceso permitido\n");
+    else
+        printf("Acceso denegado\n");
+}
