80858e59cd8487a62a0e1e64f5c1d4f55ffd76f3
Mariana-kiki <acosmariana9@outlook.com>
2025-10-24 07:09:04 -0600

    ejercici1.c

--- ejercici1.c
+++ ejercici1.c
@@ -2,3 +2,15 @@
 clase 2 ejercicio1*/
 
 #include <stdio.h>
+void main ()
+{
+  int num;
+
+  printf("Dame un numero: \n");
+  scanf("%d", &num);
+
+  if (num > 100)
+{
+    printf("El numero es mayor a 100");
+}
+}
